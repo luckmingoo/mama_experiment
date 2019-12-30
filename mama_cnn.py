9ccfc0f7db04c08c7b586d9c6b6f5c6109969fd5
mingo <luckmingoo@163.com>
2019-12-30 16:07:25 +0800

    update code daily

--- mama_cnn.py
+++ mama_cnn.py
@@ -53,8 +53,8 @@ def get_test_data(dataset, test_id, method, save_feature_dict, root_dir_prefix, 
             label = int(row[0])
             if label == 2:
                 continue
-            elif label == 0:
-                label = -1  # change benign label to -1
+#             elif label == 0:
+#                 label = -1  # change benign label to -1
             seq_file_path = row[1]
             md5 = seq_file_path.split('/')[1]
             test_data_md5.append([md5, label])
@@ -96,8 +96,8 @@ def get_train_data(dataset, method, save_feature_dict, root_dir_prefix, device_s
             label = int(row[0])
             if label == 2:
                 continue
-            elif label == 0:
-                label = -1  # change benign label to -1
+#             elif label == 0:
+#                 label = -1  # change benign label to -1
             seq_file_path = row[1]
             md5 = seq_file_path.split('/')[1]
             train_data_md5.append([md5, label])
@@ -127,15 +127,6 @@ def get_train_data(dataset, method, save_feature_dict, root_dir_prefix, device_s
     x_train = x_train[s]
     y_train = y_train[s]
     return x_train, y_train
-
-def classify(pred):
-    pred_len = pred.shape[0]
-    for i in range(pred_len):
-        if pred[i] >= 0.5:
-            pred[i] = 1
-        else:
-            pred[i] = -1
-    return pred
             
 def evaluation(method, dataset, user, device_source): 
     log_name = 'log/cnn_%s_%s_evaluation.txt' % (dataset, method)
@@ -152,16 +143,15 @@ def evaluation(method, dataset, user, device_source):
     print('x_train shape: %s y_train shape: %s' % (str(x_train.shape), str(y_train.shape)))
     start = time.time()
     print('start train')
-    clf = CNN(layer_num = 3, kernel_size = 5, gpu_id = 1)
+    clf = CNN(layer_num = 3, kernel_size = 3, gpu_id = 2)
     clf.fit(x_train, y_train, epoch = 5, batch_size = 500, lr = 0.01)
     end = time.time()
     print('Training  model time used: %f s' % (end - start))
 #     torch.cuda.empty_cache()
     print(x_train.shape)
-    y_pred = clf.predict(x_train, batch_size = 500)
+    y_pred = clf.predict(x_train, batch_size = 20)
     print(y_pred.shape)
-    y_pred = classify(y_pred)
-    cm = confusion_matrix(y_train, y_pred)
+    cm = confusion_matrix(y_train, np.int32(y_pred >= 0.5))
     TP = cm[1][1]
     FP = cm[0][1]
     TN = cm[0][0]
@@ -172,11 +162,11 @@ def evaluation(method, dataset, user, device_source):
         f.write('train data TP FP TN FN F1: %d %d %d %d %.4f\n' % (TP, FP, TN, FN, F1))
     x_train = []
     y_train = []
-    for test_id in range(0, 13):
+    for test_id in range(0, 1):#13):
         x_test, y_test = get_test_data(dataset, test_id, method, save_feature_dict, root_dir_prefix, device_source)
         print('x_test shape: %s y_test shape: %s' % (str(x_test.shape), str(y_test.shape)))
         y_pred = clf.predict(x_test, batch_size = 500)
-        y_pred = classify(y_pred)
+#         y_pred = classify(y_pred)
         cm = confusion_matrix(y_test, y_pred)
         TP = cm[1][1]
         FP = cm[0][1]
@@ -187,11 +177,60 @@ def evaluation(method, dataset, user, device_source):
         with open(log_name, 'a') as f:
             f.write('test_id %d TP FP TN FN F1: %d %d %d %d %.4f\n' % (test_id, TP, FP, TN, FN, F1))
 
+def optimize_para(method, dataset, user, device_source):
+    log_name = 'log/optimize_cnn_%s_%s_evaluation.txt' % (dataset, method)
+    if os.path.exists(log_name):
+        os.remove(log_name)
+    if user == 'mlsnrs':
+        root_dir_prefix = '/home/mlsnrs/apks'
+    elif user == 'shellhand':
+        root_dir_prefix = '/mnt'
+    save_feature_path = '%s/%s/mamadroid/%s/%s/%s_save_feature_list.csv' % (root_dir_prefix, device_source, dataset, method, method)
+    save_feature_dict = get_save_feature_dict(save_feature_path)
+    print('have read save_feature_dict: %d' % len(save_feature_dict))
+    x_train, y_train = get_train_data(dataset, method, save_feature_dict, root_dir_prefix, device_source)
+    print('x_train shape: %s y_train shape: %s' % (str(x_train.shape), str(y_train.shape)))
+    start = time.time()
+    print('start train')
+    for k in [3, 5]:
+        clf = CNN(layer_num = 3, kernel_size = k, gpu_id = 2)
+        for e in range(30, 300, 10):
+            clf.fit(x_train, y_train, epoch = e, batch_size = 500, lr = 0.01)
+            end = time.time()
+            print('Training kernel_size=%d  epoch=%d time used: %f s' % (k, e, end - start))
+        #     torch.cuda.empty_cache()
+            y_pred = clf.predict(x_train, batch_size = 20)
+            cm = confusion_matrix(y_train, np.int32(y_pred >= 0.5))
+            TP = cm[1][1]
+            FP = cm[0][1]
+            TN = cm[0][0]
+            FN = cm[1][0]
+            F1 = float(2*TP)/(2*TP + FN + FP)
+            print('train data kernel_size=%d  epoch=%d TP FP TN FN F1: %d %d %d %d %.4f' % (k, e, TP, FP, TN, FN, F1))
+            with open(log_name, 'a') as f:
+                f.write('train data kernel_size=%d  epoch=%d TP FP TN FN F1: %d %d %d %d %.4f\n' % (k, e, TP, FP, TN, FN, F1))
+            x_train = []
+            y_train = []
+            for test_id in range(0, 1):#13):
+                x_test, y_test = get_test_data(dataset, test_id, method, save_feature_dict, root_dir_prefix, device_source)
+#                 print('x_test shape: %s y_test shape: %s' % (str(x_test.shape), str(y_test.shape)))
+                y_pred = clf.predict(x_test, batch_size = 500)
+        #         y_pred = classify(y_pred)
+                cm = confusion_matrix(y_test, np.int32(y_pred >= 0.5))
+                TP = cm[1][1]
+                FP = cm[0][1]
+                TN = cm[0][0]
+                FN = cm[1][0]
+                F1 = float(2*TP)/(2*TP + FN + FP)
+                print('test_id %d kernel_size=%d  epoch=%d TP FP TN FN F1: %d %d %d %d %.4f' % (k, e, test_id, TP, FP, TN, FN, F1))
+                with open(log_name, 'a') as f:
+                    f.write('test_id %d kernel_size=%d  epoch=%d TP FP TN FN F1: %d %d %d %d %.4f\n' % (k, e, test_id, TP, FP, TN, FN, F1))
 
 if __name__ == "__main__":
     method = args.method
     dataset = args.dataset
     user = args.user
     device_source = args.device_source
-    evaluation(method, dataset, user, device_source)
+#     evaluation(method, dataset, user, device_source)
+    optimize_para(method, dataset, user, device_source)
     print('finish')
